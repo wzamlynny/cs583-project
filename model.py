d1188f1e14d56efcb266d66126331926a80da98a
Christopher Hittner <chrishittner@gmail.com>
2019-03-25 13:06:17 -0400

    First version of training/models

---
+++ model.py
@@ -0,0 +1,80 @@
+
+from keras.models import Sequential, Model
+from keras.layers import *
+
+class KaggleModel:
+    def __init__(self, model, train, test):
+        model.summary()
+        self.model = model
+        self.train_data = train
+        self.test_data = test
+
+    def compile(self):
+        """ Compiles the model. Should be defined by the user.
+        """
+        raise NotImplementedError()
+
+    def train(self, epochs=1):
+        """ Default training behavior. Simply does a Model.fit(X, Y).
+        """
+        # Get the training data
+        X_train, Y_train = self.train_data
+        
+        # Fit to the data
+        self.model.fit(X_train, Y_train, epochs=epochs, validation_data=self.test_data)
+
+    def predict(self, X):
+        return self.model.predict(X)
+
+class ImageFreeModel(KaggleModel):
+    def __init__(self, train, test):
+        model = Sequential(name='image_free')
+        
+        model.add(Dense(32, activation='sigmoid', input_shape=(32,)))
+
+        # Labels are one of [0, 1, 2, 3, 4]
+        model.add(Dense(5, activation='softmax'))
+        
+        # Build using the built model
+        super().__init__(model, train, test)
+
+    def compile(self):
+        self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+class ConvModel(KaggleModel):
+    def __init__(self, train, test):
+        # The model takes in attributes and an image.
+        attr = Input(shape=(32,))
+        img = Input(shape=(64, 64, 3))
+        
+        # Architecture for the attributes
+        y_attr = attr
+        y_attr = Dense(32, activation='relu')(y_attr)
+        y_attr = Dense(32, activation='relu')(y_attr)
+
+        # Architecture for the images
+        y_img = img
+
+        while y_img.shape[1] > 8:
+            y_img = Conv2D(64, kernel_size=(5,5), strides=(1,1), activation='relu', padding='same')(y_img)
+            y_img = Conv2D(64, kernel_size=(5,5), strides=(1,1), activation='relu', padding='same')(y_img)
+            y_img = MaxPooling2D(pool_size=(2,2))(y_img)
+        
+        y_img = AveragePooling2D(pool_size=(y_img.shape[1], y_img.shape[2]))(y_img)
+        y_img = Reshape((64,))(y_img)
+        y_img = Dense(1024, activation='relu')(y_img)
+        y_img = Dense(32, activation='relu')(y_img)
+
+        # Final processing
+        y = Concatenate()([y_attr, y_img])
+        y = Dense(32, activation='sigmoid')(y)
+        y = Dense(5, activation='softmax')(y)
+
+        model = Model([attr, img], y)
+        model.name = 'conv_model'
+
+        super().__init__(model, train, test)
+
+if __name__ == '__main__':
+    mdl = ConvModel(None, None)
+
---
+++ train.py
@@ -0,0 +1,58 @@
+
+from model import *
+from data import *
+
+from keras.utils import to_categorical
+
+import numpy as np
+
+# Get the data
+X, Y = load_train_data()
+
+#X = X[0] # For non-image training, just use the labels
+
+# For single image training, just use the first image
+for i in range(len(X[1])):
+    if len(X[1][i]) > 0:
+        X[1][i] = X[1][i][0]
+    else:
+        X[1][i] = np.zeros((64,64,3))
+
+X[1] = np.array(X[1])
+
+# Shuffle the data
+idxs = list(range(len(Y)))
+X = X[idxs]
+Y = Y[idxs]
+
+# Display the distribution of each possible output
+hist = np.histogram(Y, bins=[0,1,2,3,4,5])
+print(hist)
+for i in range(5):
+    print('Num w/ label', i, ':', hist[0][i])
+print('Total:', len(Y))
+
+# One hot encode the output
+Y = to_categorical(Y)
+
+# Validation split
+s = int(0.2 * len(X))
+X_train = X[:-s]
+Y_train = Y[:-s]
+X_valid = X[-s:]
+Y_valid = Y[-s:]
+
+print('Training points:', len(X_train))
+print('Validation points:', len(X_valid))
+print('Total points:', len(X))
+
+# Build a model
+#clf = ImageFreeModel((X_train, Y_train), (X_valid, Y_valid))
+clf = ConvModel((X_train, Y_train), (X_valid, Y_valid))
+
+# Build the model
+clf.compile()
+
+# Fit to the data
+clf.train(epochs=32)
+
